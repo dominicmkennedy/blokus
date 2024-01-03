b9bbbdb1cd681a1ea1fc93726c2a65df5719cc68
dom <dominicmkennedy@gmail.com>
2024-01-03 13:16:04 -0500

    move polyomino into class

--- blokus.py
+++ blokus.py
@@ -1,47 +1,71 @@
 import numpy as np
-
-empty_board = np.zeros((20, 20))
-
-red_corners = [(0, 0)]
-blu_corners = [(0, -1)]
-grn_corners = [(-1, 0)]
-ylw_corners = [(-1, -1)]
-
-polyomino = list[tuple[int, int]]
+import numpy.typing as npt
 
 
-def flip_poly():
-    pass
+coord = tuple[int, int]
+
+empty_board = np.zeros((20, 20), dtype=np.int8)
+player_corners = {
+    "red": set([(0, 0)]),
+    "blu": set([(0, -1)]),
+    "grn": set([(-1, 0)]),
+    "ylw": set([(-1, -1)]),
+}
+
+
+class Polyomino:
+    def __init__(self, squares: set[coord]) -> None:
+        self.squares = squares
+
+    def footprint(self) -> tuple[int, int]:
+        mx = max(self.squares, key=lambda x: x[0])[0]
+        my = max(self.squares, key=lambda x: x[1])[1]
+
+        return (min(mx, my), max(mx, my))
+
+    def flip_x(self):
+        mx = max(self.squares, key=lambda x: x[0])[0]
+        self.squares = set(map(lambda x: (-x[0], x[1]), self.squares))
+        self.translate((mx, 0))
+
+        return self
+
+    def translate(self, rel: coord):
+        self.squares = set(map(lambda x: (x[0] + rel[0], x[1] + rel[1]), self.squares))
+        return self
 
 
 # https://i.ebayimg.com/images/g/cj8AAOSwdXhekQcT/s-l1200.jpg
-polyominos: list[polyomino] = [
-    [(0, 0)],  # 1
-    [(0, 0), (0, 1)],  # 2
-    [(0, 0), (0, 1), (0, 2)],  # 3
-    [(0, 0), (0, 1), (1, 0)],  # 4
-    [(0, 0), (0, 1), (0, 2), (0, 3)],  # 5
-    [(0, 0), (1, 0), (1, 1), (1, 2)],  # 6
-    [(0, 0), (0, 1), (1, 1), (0, 2)],  # 7
-    [(0, 0), (0, 1), (1, 0), (1, 1)],  # 8
-    [(0, 1), (1, 1), (1, 0), (2, 0)],  # 9
-    [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)],  # 10
-    [(0, 0), (1, 0), (1, 1), (1, 2), (1, 3)],  # 11
-    [(0, 0), (0, 1), (1, 1), (1, 2), (1, 3)],  # 12
-    [(0, 0), (0, 1), (1, 0), (1, 1), (1, 2)],  # 13
-    [(0, 0), (1, 0), (1, 1), (1, 2), (0, 2)],  # 14
-    [(0, 0), (0, 1), (0, 2), (1, 2), (0, 3)],  # 15
-    [(0, 0), (1, 0), (2, 0), (1, 1), (1, 2)],  # 16
-    [(0, 0), (0, 1), (0, 2), (1, 0), (2, 0)],  # 17
-    [(0, 2), (1, 2), (1, 1), (2, 1), (2, 0)],  # 18
-    [(0, 1), (0, 2), (1, 1), (2, 1), (2, 0)],  # 19
-    [(0, 1), (0, 2), (1, 1), (1, 0), (2, 1)],  # 20
-    [(0, 1), (1, 1), (1, 0), (1, 2), (2, 1)],  # 21
+polyominos: list[Polyomino] = [
+    Polyomino(set([(0, 0)])),  # 1
+    Polyomino(set([(0, 0), (0, 1)])),  # 2
+    Polyomino(set([(0, 0), (0, 1), (0, 2)])),  # 3
+    Polyomino(set([(0, 0), (0, 1), (1, 0)])),  # 4
+    Polyomino(set([(0, 0), (0, 1), (0, 2), (0, 3)])),  # 5
+    Polyomino(set([(0, 0), (1, 0), (1, 1), (1, 2)])),  # 6
+    Polyomino(set([(0, 0), (0, 1), (1, 1), (0, 2)])),  # 7
+    Polyomino(set([(0, 0), (0, 1), (1, 0), (1, 1)])),  # 8
+    Polyomino(set([(0, 1), (1, 1), (1, 0), (2, 0)])),  # 9
+    Polyomino(set([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)])),  # 10
+    Polyomino(set([(0, 0), (1, 0), (1, 1), (1, 2), (1, 3)])),  # 11
+    Polyomino(set([(0, 0), (0, 1), (1, 1), (1, 2), (1, 3)])),  # 12
+    Polyomino(set([(0, 0), (0, 1), (1, 0), (1, 1), (1, 2)])),  # 13
+    Polyomino(set([(0, 0), (1, 0), (1, 1), (1, 2), (0, 2)])),  # 14
+    Polyomino(set([(0, 0), (0, 1), (0, 2), (1, 2), (0, 3)])),  # 15
+    Polyomino(set([(0, 0), (1, 0), (2, 0), (1, 1), (1, 2)])),  # 16
+    Polyomino(set([(0, 0), (0, 1), (0, 2), (1, 0), (2, 0)])),  # 17
+    Polyomino(set([(0, 2), (1, 2), (1, 1), (2, 1), (2, 0)])),  # 18
+    Polyomino(set([(0, 1), (0, 2), (1, 1), (2, 1), (2, 0)])),  # 19
+    Polyomino(set([(0, 1), (0, 2), (1, 1), (1, 0), (2, 1)])),  # 20
+    Polyomino(set([(0, 1), (1, 1), (1, 0), (1, 2), (2, 1)])),  # 21
 ]
 
 
-def max_poly_dim(p: polyomino) -> tuple[int, int]:
-    mx = max(p, key=lambda x: x[0])
-    my = max(p, key=lambda x: x[1])
+def valid_move(
+    b: npt.NDArray[np.int8], b_coord: coord, p: Polyomino, p_coord: coord
+) -> bool:
+    print(p.squares)
+    p.translate(b_coord)
+    print(p.squares)
 
-    return (mx[0], my[1])
+    return True
